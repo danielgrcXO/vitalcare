9ce6579836b2340597ea9e964e5ccfa3fbacf747
Israel18Islas <tomas.islas@uabc.edu.mx>
2022-07-18 00:39:08 -0700

    completed styles appended for new patient

--- src/app/patient/new/new.component.ts
+++ src/app/patient/new/new.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { faCake } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-new',
@@ -7,9 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public register(){
+    alert("Registered Successfully");
+    this.router.navigateByUrl('/home');
+  }
+
+  UserIcon = faUser;
+  Place = faBuilding;
+  Email = faEnvelope;
+  Phone = faPhone;
+  Location = faLocation;
+  Birthday = faCake;
 }
